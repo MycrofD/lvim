4709c3df5f6c44379112bd0f16056a1d23183d3a
auro.mohanty <aurogreenindia@gmail.com>
2024-06-15 00:10:26 +0200

    feat: add LazyDev and 2 more supporting plugins

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -268,6 +268,30 @@ lvim.plugins = {
       telescope.load_extension("live_grep_args")
     end
   },
+  -- LazyDev - Faster LuaLS setup for Neovim
+  -- and other helpers as per README
+  {
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "luvit-meta/library", words = { "vim%.uv" } },
+      },
+    },
+  },
+  { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
+  {                                        -- optional completion source for require statements and module annotations
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      opts.sources = opts.sources or {}
+      table.insert(opts.sources, {
+        name = "lazydev",
+        group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+      })
+    end,
+  },
   -- MUST HAVES -- Should probably be installed via Mason
   {
     "williamboman/mason.nvim",
