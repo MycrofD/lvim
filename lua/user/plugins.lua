114546955a9c8c11faffe1af2998fbf6ba3f7d07
auro.mohanty <aurogreenindia@gmail.com>
2024-06-14 22:33:44 +0200

    remove FixCursorHold plugin as it is not required anymore per its doc

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -85,7 +85,6 @@ lvim.plugins = {
     dependencies = {
       "nvim-neotest/nvim-nio",
       "nvim-lua/plenary.nvim",
-      "antoinemadec/FixCursorHold.nvim",
       "nvim-treesitter/nvim-treesitter"
     },
     config = function()
